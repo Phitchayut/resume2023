4e6c13714343d4dd795436b320982bb71cbf9197
phitchayut <70578824+Phitchayut@users.noreply.github.com>
2023-08-02 06:34:20 +0000

    Edit Link Contact Me

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
+import { Link } from "react-scroll";
 
 const Experience = () => {
   return (
@@ -16,12 +17,11 @@ const Experience = () => {
           >
             <h2 className="h2 text-5xl text-accent">Experience</h2>
             <h3 className="h2 text-xl mb-4 text-gradient">
-              Federation of Accouting Professions <span className="font-thai">(สภาวิชาชีพบัญชี)</span>
+              Federation of Accouting Professions{" "}
+              <span className="font-thai">(สภาวิชาชีพบัญชี)</span>
             </h3>
             <div className="backdrop-blur-sm bg-white/30 rounded-full text-center lg:px-3 w-full lg:w-2/4">
-              <h3 className="h3 mb-4 text-gradient">
-                2016 - Present
-              </h3>
+              <h3 className="h3 mb-4 text-gradient">2016 - Present</h3>
             </div>
             <div className="flex flex-col  mb-12">
               <p className="h3 text-xl">
@@ -37,7 +37,15 @@ const Experience = () => {
               </p>
             </div>
             <div className="flex justify-center lg:justify-start gap-x-8 items-center">
-              <button className="btn btn-lg">Hire Me</button>
+              <Link
+                to="contact"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                className="flex justify-center items-center cursor-pointer h-[60px]"
+              >
+                <button className="btn btn-lg">Hire Me</button>
+              </Link>
               <a href="#" className="text-gradient btn-link">
                 Resume
               </a>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const Header = () => {
   return (
@@ -6,14 +7,24 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           {/* logo */}
-            <a href="#">
-              <div className="flex flex-col justify-center">
-                <h1 className="text-[2rem] font-bold text-gradient btn-link">Beam</h1>
-                <p className='text-[2rem]'>Phitchayut</p>
-              </div>
-            </a>
+          <a href="#">
+            <div className="flex flex-col justify-center">
+              <h1 className="text-[2rem] font-bold text-gradient btn-link">
+                Beam
+              </h1>
+              <p className="text-[2rem]">Phitchayut</p>
+            </div>
+          </a>
           {/* button */}
-          <button className="btn btn-sm">Contact Me</button>
+          <Link
+            to="contact"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            className="flex justify-center items-center cursor-pointer h-[60px]"
+          >
+            <button className="btn btn-lg">Contact Me</button>
+          </Link>
         </div>
       </div>
     </header>
