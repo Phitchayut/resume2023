fde76273460eabff59ef0fb3bf251b62bef24478
phitchayut <70578824+Phitchayut@users.noreply.github.com>
2023-08-02 08:20:46 +0000

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className="container mx-auto h-[30vh] px-5">
+      <h1 className="text-center">Design With</h1>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
