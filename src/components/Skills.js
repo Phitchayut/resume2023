556c8aa6798865339318e9346fe2edc04e49a9aa
phitchayut <70578824+Phitchayut@users.noreply.github.com>
2023-08-02 02:31:46 +0000

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {SkillsList} from "../Data";
+import { SkillsList } from "../Data";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
@@ -18,10 +18,11 @@ const Skills = () => {
       >
         Skills
       </motion.h1>
-      <motion.div variants={fadeIn("down", 0.3)}
+      <motion.div
+        variants={fadeIn("down", 0.3)}
         initial="hidden"
         whileInView={"show"}
-        viewport={{ once: false, amount: 0.5 }}
+        viewport={{ once: false, amount: 0.3 }}
         className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 text-center capitalize"
       >
         <div className="border-0 lg:border-r-4 lg:border-indigo-500/75">
@@ -117,11 +118,11 @@ const Skills = () => {
         </div>
         {/* end-backend */}
         <div className="border-0 lg:border-l-4 lg:border-indigo-500/75">
-        <div className="h3 text-3xl text-gradient">Others</div>
-        <p className="text-rose-500/75 font-semibold text-xl tracking-wide italic">
+          <div className="h3 text-3xl text-gradient">Others</div>
+          <p className="text-rose-500/75 font-semibold text-xl tracking-wide italic">
             Basic
           </p>
-        <motion.div
+          <motion.div
             variants={fadeIn("left", 0.4)}
             initial="hidden"
             whileInView={"show"}
