05e25c923183b4077bd9993f5b3f5b2193ec0199
phitchayut <70578824+Phitchayut@users.noreply.github.com>
2023-08-03 06:33:02 +0000

    Update 11 files

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import CountUp from "react-countup";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
 const About = () => {
   const [ref, inView] = useInView({
@@ -27,26 +27,46 @@ const About = () => {
             viewport={{ once: false, amount: 0.3 }}
             className="flex-1"
           >
-            <h2 className="h2 text-5xl text-accent">About Me</h2>
-            <h3 className="h3 mb-4">
+            <h2 className="h2 text-5xl text-accent text-center lg:text-left">
+              About Me
+            </h2>
+            <h3 className="h3 mb-4 text-center lg:text-left">
               I'm a Front-end Developer.
             </h3>
-            <div className="flex flex-col  mb-12">
-              <p className="h3 text-xl">My Name is: <span className="text-gradient text-3xl">Phitchayut Chanpherng</span></p>
-              <p className="h3 text-xl">Nick Name: <span className="text-gradient text-3xl">Beam</span></p>
-              <p className="h3 text-xl">My Birth date: <span className="text-gradient text-3xl">10 November 1995</span></p>
-              <h1 className="h2 text-5xl text-gradient">{inView ? <CountUp start={0} end={28} duration={3} /> : null} <span className="text-lg">Year Old</span></h1>
+            <div className="flex flex-col mb-12 text-center lg:text-left">
+              <p className="h3 text-xl">
+                My Name is:{" "}
+                <span className="text-gradient text-[27px] block lg:inline-block">
+                  Phitchayut Chanpherng
+                </span>
+              </p>
+              <p className="h3 text-xl">
+                Nick Name:{" "}
+                <span className="text-gradient text-3xl block lg:inline-block">
+                  Beam
+                </span>
+              </p>
+              <p className="h3 text-xl">
+                My Birth date:{" "}
+                <span className="text-gradient text-3xl block lg:inline-block">
+                  10 November 1995
+                </span>
+              </p>
+              <h1 className="h2 text-5xl text-gradient text-center lg:text-left">
+                {inView ? <CountUp start={0} end={28} duration={3} /> : null}{" "}
+                <span className="text-lg">Year Old</span>
+              </h1>
             </div>
             <div className="flex justify-center lg:justify-start gap-x-8 items-center">
               <Link
-            to="contact"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            className="flex justify-center items-center cursor-pointer h-[60px]"
-          >
-            <button className="btn btn-lg">Hire Me</button>
-          </Link>
+                to="contact"
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                className="flex justify-center items-center cursor-pointer h-[60px]"
+              >
+                <button className="btn btn-lg">Hire Me</button>
+              </Link>
               <a href="#" className="text-gradient btn-link">
                 Resume
               </a>
